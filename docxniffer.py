30cefa53ab4d3c3e117c935b7a70aa6cd8e953fa
Bavo Meyvis <bavomeyvis@gmail.com>
2021-04-16 22:25:33 +0200

    Well, the party was nice, the party was pumpin'

--- docxniffer.py
+++ docxniffer.py
@@ -1,57 +1,55 @@
-import mammoth
 import argparse
+import mammoth
 import os
 from pathlib import Path
 from zipfile import ZipFile
 from zipfile import BadZipfile
 
-pathroot = os.getcwd()
 
-#def docx_sniffer():
-
-
-
-def conv_docx(input_path, output_path, filetype):
-    # Set the starting directory for the input
-    print('input_path = %s'%input_path)
-    print('output_path = %s'%output_path)
-    print('filetype = %s'%filetype)
-    
-    result = ''
+def docxniffer(input_path):
+    sources = []
     for dirName, subdirList, fileList in os.walk(input_path):
-        print("Entered directory %s" % dirName)
         for fname in fileList:
             if fname.endswith('.docx'):
-                
+                full_path = dirName+'/'+fname
+                sources.append(full_path)
+    for source in sources:
+        print(source)
+    return sources
 
-                # output_path = ../../AFolder/AnotherFolder
 
+def dogwhisperer(input_path, output_path, method):
+    print('Running script with arguments: \n\t{0} (input)\n\t{1}(output)\n\t{2}Not implemented (method)\n'.format(input_path, output_path, method))
 
-                # Get the path of input file
-                source_file_path = dirName+'/'+fname
+    sources = docxniffer(input_path)
+    user_input = input("\nThe listed files are to be converted to markdown-files relative to %s\nPress any key to continue..."%output_path)
+    
+    result = ''
+    for source in sources:
+        print('Converting: %s...' % source)
+        try:
+            with ZipFile(source) as zf:
+                # Temporarily save the content
+                with open(source, "rb") as docx_file:
+                    result = mammoth.convert_to_markdown(docx_file)
+                # Determine whether to use relative or absolute path for output file
                 destination_file_path = ''
-                try:
-                    # Open input docx and save output file
-                    with ZipFile(source_file_path) as zf:
-                        print('Converting: %s\n' % source_file_path)
-                        with open(source_file_path, "rb") as docx_file:
-                            result = mammoth.convert_to_markdown(docx_file)
-                            # Output to correct path with new extension
-                        if input_path == output_path:
-                            destination_file_path = Path(source_file_path).with_suffix(filetype)
-                        else:
-                            new_path = output_path+'/'+fname
-                            destination_file_path = Path(new_path).with_suffix(filetype)
-                        print('Saving %s'%destination_file_path)
-                        with open(destination_file_path, "w") as markdown_file:
-                            markdown_file.write(result.value)
-                except BadZipfile:
-                    print("[EXCEPT]: BadZipFile. Skipping %s...\n"%fname)
+                if input_path == output_path:
+                    destination_file_path = Path(source).with_suffix('.md')
+                else:
+                    fname = os.path.basename(source) # TODO: Replace hacky solution
+                    new_path = output_path+'/'+fname
+                    destination_file_path = Path(new_path).with_suffix('.md')
+                # Output content with new filetype to determined path 
+                with open(destination_file_path, "w") as markdown_file:
+                    markdown_file.write(result.value)
+        except BadZipfile:
+            print("[EXCEPT]: BadZipFile. Skipping %s...\n" % source)
+            # File might: be corrupt, empty or not on path
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Docx converter")
-
     parser.add_argument("--input_path",
                         help="The folder that needs to be searched. Defaults to where script is run.",
                         nargs='?',
@@ -60,9 +58,9 @@ if __name__ == '__main__':
                         help="Save location",
                         nargs='?',
                         default=".")
-    parser.add_argument("--filetype",
-                    help="Filetype to convert to. E.g. '.md'",
+    parser.add_argument("--method",
+                    help="[Not yet implemented]",
                     nargs='?',
-                    default=".md")
+                    default="")
     args = parser.parse_args()
-    conv_docx(args.input_path, args.output_path, args.filetype)
+    dogwhisperer(args.input_path, args.output_path, args.method)
